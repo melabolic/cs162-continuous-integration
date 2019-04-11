d5b7f1e058dcb1b1a50e69ed36cdafa4f0bc8264
melabolic <jiewen.lim@minerva.kgi.edu>
2019-04-11 08:44:41 -0300

    adding the integration test

---
+++ tests/test.py
@@ -0,0 +1,29 @@
+from app import app, db
+import unittest
+
+class TestDatabase(unittest.TestCase):
+
+    def setup(self):
+        # link to the db
+        pass
+
+     def test_add_valid(self):
+        response = self.app.post(
+          '/add',
+          data = {"form": {"expression":"1+1"}},
+          follow_redirects=True
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_invalid(self):
+       response = self.app.post(
+         '/add',
+         data = {"form": {"expression":"1+"}},
+         follow_redirects=True
+       )
+       self.assertEqual(response.status_code, 400)
+
+
+    def tearDown(self):
+        # tear down the db
+        pass
