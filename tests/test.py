0f0250b3bc0b97f4fc7ebc02c4aa4bdc6bc0e52c
melabolic <jiewen.lim@minerva.kgi.edu>
2019-04-11 09:15:36 -0300

    updated indentation, again...

--- tests/test.py
+++ tests/test.py
@@ -7,9 +7,9 @@ class TestDatabase(unittest.TestCase):
         # link to the db
         pass
 
-     def test_add_valid(self):
-         response = self.app.post('/add', data = {"form": {"expression":"1+1"}},follow_redirects=True)
-         self.assertEqual(response.status_code, 200)
+    def test_add_valid(self):
+        response = self.app.post('/add', data = {"form": {"expression":"1+1"}},follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_add_invalid(self):
         response = self.app.post('/add', data = {"form": {"expression":"1+"}},follow_redirects=True)
