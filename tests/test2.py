a7300a8a0a93e6452bf43fae8f8947a6ffcf1ff0
melabolic <jiewen.lim@minerva.kgi.edu>
2019-04-11 10:45:06 -0300

    added requests module

--- tests/test2.py
+++ tests/test2.py
@@ -1,21 +1,21 @@
 # code take from https://gist.github.com/kevinyang372/252c0f5a0d37fbef478d6774ef185603?fbclid=IwAR1bGvZwDy16Wod6dnERFKed1qJGfROgscJmJEavOCDbj0lqoAIfUmqRFdw
 import os
-from flask import request
+import requests
 import unittest
 from sqlalchemy import create_engine
 
 class DockerComposeTestCase(unittest.TestCase):
 
     def test_endpoint(self):
-        r = request.post('http://127.0.0.1:5000/add', data={'expression':'100+100'})
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+100'})
         self.assertEqual(r.status_code, 200)
 
     def test_error_endpoint(self):
-        r = request.post('http://127.0.0.1:5000/add', data={'expression':'100+'})
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+'})
         self.assertNotEqual(r.status_code, 200)
 
     def test_db(self):
-        r = request.post('http://127.0.0.1:5000/add', data={'expression':'100+100'})
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+100'})
         engine = create_engine('postgresql://cs162_user:cs162_password@127.0.0.1:5432/cs162', echo = True)
 
         with engine.connect() as con:
@@ -25,7 +25,7 @@ class DockerComposeTestCase(unittest.TestCase):
         self.assertNotEqual(len(rows), 0)
 
     def test_error_db(self):
-        r = request.post('http://127.0.0.1:5000/add', data={'expression':'100+'})
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression':'100+'})
         engine = create_engine('postgresql://cs162_user:cs162_password@127.0.0.1:5432/cs162', echo = True)
 
         with engine.connect() as con:
